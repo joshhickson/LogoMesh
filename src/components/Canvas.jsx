9a0f83d476f51ecc978025cf22af8fc9b9cbc368
joshhickson <84687634+joshhickson@users.noreply.github.com>
2025-05-04 02:58:40 +0000

    Restored to 'ebc926be85b114230a41672822c5517fa0a5cb50'
    Replit-Restored-To: ebc926be85b114230a41672822c5517fa0a5cb50

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactFlow, { 
   MiniMap, 
@@ -50,14 +51,27 @@ function Canvas({ thoughts, setSelectedThought, activeFilters }) {
     },
   }));
 
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+  const onNodeClick = (event, node) => {
+    const thought = thoughts.find(t => t.thought_bubble_id === node.id);
+    if (thought) setSelectedThought(thought);
+  };
+
   return (
-    <div style={{ width: '100%', height: '100vh' }}>
-      <ReactFlow 
-        nodes={initialNodes}
-        onNodeClick={(_, node) => setSelectedThought(node.id)}
+    <div style={{ width: '100%', height: '100%' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onNodeClick={onNodeClick}
+        fitView
       >
-        <Controls />
         <Background />
+        <Controls />
+        <MiniMap />
       </ReactFlow>
     </div>
   );
